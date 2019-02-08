d466918180b85bd49d392a7869ac0798c5e8710e
Tatsuya TOJO <e185707@ie.u-ryukyu.ac.jp>
2019-02-08 19:09:17 +0900

    Janken.javaを実装しました。

---
+++ src/main/java/jp/ac/uryukyu/ie/e185707/Janken.java
@@ -0,0 +1,39 @@
+package jp.ac.uryukyu.ie.e185707;
+
+public class Janken {
+    static String[] handList = {"グー","チョキ","パー"};
+
+    static void startMessage() {
+        System.out.println("じゃんけんをするよ！");
+    }
+
+    static void endMessage() {
+        System.out.println("ありがとう！また遊ぼうね！");
+    }
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        CPU cpu = new CPU();
+        Judge judge = new Judge();
+
+        // 開始の挨拶
+        startMessage();
+
+            // CPUが手をランダムに作成
+            cpu.setHand();
+            // Playerが手を入力
+            player.setHand();
+            // Player,CPUの手を表示
+            System.out.println("・あなたの手：" + handList[player.getHand()]);
+            System.out.println("・わたしの手：" + handList[cpu.getHand()]);
+            // 判定
+            judge.judgement(cpu.getHand(), player.getHand());
+         // 繰り返し終わり
+        // 結果を表示
+        //judge.result();
+        // 終了の挨拶
+        endMessage();
+    }
+}
+
+
